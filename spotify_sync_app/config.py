a7a8a888223ccdf589477d078b1a7dfd0722be20
Daniel Redly <dredly@gmail.com>
2023-02-27 22:38:28 +0000

    can now obtain access and refresh tokens

--- spotify_sync_app/config.py
+++ spotify_sync_app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
 SCOPES = "playlist-modify-private playlist-modify-public"
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
--- spotify_sync_app/routes.py
+++ spotify_sync_app/routes.py
@@ -1,6 +1,7 @@
 from spotify_sync_app import app
-from flask import redirect
-from .config import AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI, SCOPES
+from flask import redirect, request, url_for
+from .config import AUTHORIZE_URL, CLIENT_ID, SCOPES, CLIENT_SECRET, TOKEN_URL
+import requests
 
 # Health check to quickly verify if the API is running or not
 @app.route("/")
@@ -12,10 +13,27 @@ def login():
     return redirect(AUTHORIZE_URL 
         + "?response_type=code" 
         + "&client_id=" + CLIENT_ID
-        + "&redirect_uri=" + REDIRECT_URI
+        + "&redirect_uri=" + url_for("callback", _external=True)
         + "&scope=" + SCOPES
     )
 
 @app.route("/callback")
 def callback():
-    return {"callback": "success"}
+    auth_code = request.args.get('code', None)
+    if not auth_code:
+        return {"error": "could not find auth code"}, 401
+
+    token_resp = requests.post(TOKEN_URL, {
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": url_for("callback", _external=True)
+        },
+        auth=(CLIENT_ID, CLIENT_SECRET)
+    )
+
+    token_resp.raise_for_status()
+    print(token_resp.json())
+    return {
+        "access_token": token_resp.json()["access_token"],
+        "refresh_token": token_resp.json()["refresh_token"]
+    }
